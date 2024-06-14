5eb6e0c580c8139bf2af7a7889f7903fe5411507
quaziii9 <quaziii9gamedev@gmail.com>
2024-06-14 12:01:52 +0900

    [init] TempProfileViewModel

--- Assets/Scripts/View/TempProfileUIView.cs
+++ Assets/Scripts/View/TempProfileUIView.cs
@@ -6,13 +6,24 @@ public class TempProfileUIView : MonoBehaviour
     [SerializeField] Text Text_Name;
     [SerializeField] Text Text_Level;
 
-    private TempProfileUIViewModel _vm;
+    private TempProfileViewModel _vm;
 
-    private void OnEnable()
-    {
-        if(_vm == null)
-        {
-            _vm
-        }
-    }
+    //private void OnEnable()
+    //{
+    //    if(_vm == null)
+    //    {
+    //        _vm = new TempProfileViewModel();
+    //        _vm.PropertyChanged += OnPropertyChanged;
+    //        _vm.ResiterEventsOnEnable();
+    //        _vm.RefreshViewModel();
+    //    }
+    //}
+
+    //private void OnDisable()
+    //{
+    //    if(_vm != null)
+    //    {
+    //        _
+    //    }
+    //}
 }
--- Assets/Scripts/ViewModel/TempProfileViewModel.cs
+++ Assets/Scripts/ViewModel/TempProfileViewModel.cs
@@ -1,16 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using ViewModel;
 
-public class TempProfileViewModel : MonoBehaviour
+public class TempProfileViewModel : ViewModelBase
 {
-    void Start()
+    private int _userId;
+    private string _name;
+    private int _level;
+    
+    public int UserId
     {
-        
+        get { return _userId; }
+        set { UserId = value; }
     }
 
-    void Update()
+    public string Name
     {
-        
+        get { return _name; }
+        set
+        {
+            if (_name == value)
+                return;
+
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    public int Level
+    {
+        get { return _level; }
+        set
+        {
+            if (_level == value)
+                return;
+
+            _level = value;
+            OnPropertyChanged(nameof(Level));
+        }
     }
 }
