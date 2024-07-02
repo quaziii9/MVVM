60841b00edb9bacef9722367e54e896ce6aca511
quaziii9 <quaziii9gamedev@gmail.com>
2024-07-02 15:21:09 +0900

    [FEAT]

---
+++ Assets/Scripts/View/CharacterStateView.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStateView : MonoBehaviour
+{
+    [SerializeField] Slider Slider_Hp;
+}
+
---
+++ Assets/Scripts/ViewModel/CharacterStateViewModel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    public class CharacterStateViewModel
+    {
+        private int _characterHp;
+
+        public int CharacterHp
+        {
+            get { return _characterHp; }
+            set
+            {
+                if (_characterHp == value)
+                    return;
+
+                _characterHp = value;
+            }
+        }
+
+        #region PropChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
+
